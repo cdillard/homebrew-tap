f367d2ab4ed8ac5715491544b9aaed520e5df727
Chris Dillard <chris.dillard@metova.com>
2016-03-25 12:37:16 -0600

    333

--- swiftlint.rb
+++ swiftlint.rb
@@ -5,6 +5,11 @@ class Swiftlint < Formula
   head "https://github.com/cdillard/SwiftLint.git"
 
   depends_on :xcode => ["7.1", :build]
+  
+  bottle do
+    cellar :any
+    sha256 "538dbfb46af97c561845e9e11aa891eed68afb43b493ab8426cad4bfc271dd7b" => :yosemite
+  end
 
   def install
     bin.install "swiftlint"

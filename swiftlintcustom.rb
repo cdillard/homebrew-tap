087396f0e433ea3d472502756991b35882ea38bb
Chris Dillard <chris.dillard@metova.com>
2016-03-25 11:07:32 -0600

    formula change

--- swiftlintcustom.rb
+++ swiftlintcustom.rb
@@ -5,12 +5,6 @@ class SwiftlintCustom < Formula
   head "https://github.com/cdillard/SwiftLint.git"
 
   depends_on :xcode => ["7.1", :build]
-  
-  bottle do
-    sha256 "89c8215f9e670819e065820a3fd59dee56c7e59f9e304f799f2b319db372f998" => :el_capitan
-    sha256 "0c8cce6920444c2b7d6ea524bc9881f2e19c6ced735bb4f833cb6e04c49c5ffc" => :yosemite
-    sha256 "0899dfe6dbc824bdf5b589020063c711c51fde9a6cca87048ec352779c2cbcf1" => :mavericks
-  end
 
   def install
     bin.install "swiftlintcustom"

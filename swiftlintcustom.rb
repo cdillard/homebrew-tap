47a50a087866ab56fb5f7fc59eb8c2e12d21a434
Chris Dillard <chris.dillard@metova.com>
2016-03-25 11:33:01 -0600

    6

--- swiftlintcustom.rb
+++ swiftlintcustom.rb
@@ -6,14 +6,14 @@ class Swiftlintcustom < Formula
 
   depends_on :xcode => ["7.1", :build]
 
-  bottle do
-    cellar :any
-    sha256 "67c80a5bda6fc050ffda404bddd99143d04a4a0a10158f526d46e4ecee4ad37b" => :el_capitan
-    sha256 "b02a94fc5e994bb939dbe425edef01cd819d31beb3126abc3f62158bafa8dc82" => :yosemite
-  end
+#  bottle do
+#    cellar :any
+#    sha256 "67c80a5bda6fc050ffda404bddd99143d04a4a0a10158f526d46e4ecee4ad37b" => :el_capitan
+#    sha256 "b02a94fc5e994bb939dbe425edef01cd819d31beb3126abc3f62158bafa8dc82" => :yosemite
+#  end
 
   def install
-    bin.install "swiftlintcustomkeg"
+    bin.install "swiftlintcustom"
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
   end
 

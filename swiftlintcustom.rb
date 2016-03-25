10e21cd354615ca2573f6b83c78c395027d49c7b
Chris Dillard <chris.dillard@metova.com>
2016-03-25 11:58:22 -0600

    use default package name

--- swiftlintcustom.rb
+++ swiftlintcustom.rb
@@ -1,4 +1,4 @@
-class Swiftlintcustom < Formula
+class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/cdillard/SwiftLint"
   url "https://github.com/cdillard/SwiftLint.git", :tag => "1.0.0", :revision => "02c38d260d1f3bb728dd372fffe614ce472b38ad"
@@ -7,12 +7,12 @@ class Swiftlintcustom < Formula
   depends_on :xcode => ["7.1", :build]
 
   def install
-    bin.install "swiftlintcustom"
+    bin.install "swiftlint"
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
   end
 
   test do
     (testpath/"Test.swift").write "import Foundation\n"
-    system "#{bin}/swiftlintcustom"
+    system "#{bin}/swiftlint"
   end
 end

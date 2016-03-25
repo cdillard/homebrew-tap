28460025537d40140835cb6c356526b05857d323
Chris Dillard <chris.dillard@metova.com>
2016-03-25 12:46:13 -0600

    777

--- swiftlint.rb
+++ swiftlint.rb
@@ -5,11 +5,6 @@ class Swiftlint < Formula
   head "https://github.com/cdillard/SwiftLint.git"
 
   depends_on :xcode => ["7.1", :build]
-  
-  bottle do
-    cellar :any
-    sha256 "538dbfb46af97c561845e9e11aa891eed68afb43b493ab8426cad4bfc271dd7b" => :el_capitan
-  end
 
   def install
     bin.install "swiftlint"
